63bffb5eea9f2f27fcaf1c78c19562b24d27d656
nari2239 <oryolife@gmail.com>
2021-03-09 22:31:18 +0900

    保存処理を追加

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -3,8 +3,18 @@ class RecordsController < ApplicationController
   end
 
   def new
+    @record = Record.new
   end
 
   def create
+    @record = Record.new(record_params)
+    if @record.save
+      redirect_to root_path
+    end
+  end
+
+  private
+  def record_params
+    params.require(:record).permit(:date, :time, :skip, :to_do)
   end
 end
